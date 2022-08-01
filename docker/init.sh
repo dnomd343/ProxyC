30ea15e0f5292e329189cd24ad0a961e4e90b4b1
dnomd343 <i@343.re>
2022-08-01 22:51:20 +0800

    update: remove legacy code

--- docker/init.sh
+++
@@ -1,8 +0,0 @@
-python /usr/local/share/ProxyC/docker/compile.py&
-/usr/bin/redis-server /etc/redis.conf
-
-dnsproxy -p 53 -u 223.5.5.5 &
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
-
-python /usr/local/share/ProxyC/Loop.py&
-python /usr/local/share/ProxyC/Web.py
