714269cc0cfa8e1946e4b45d60f2f6c03deb9d29
Dnomd343 <i@343.re>
2023-01-03 13:48:12 +0800

    feat: vless encode framework

---
+++ Encoder/VLESS.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def vless(info: dict) -> str:
+    """
+    https://github.com/XTLS/Xray-core/discussions/716
+    """
--- encoder_test.py
+++ encoder_test.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print('encoder test')
+from Filter import Filter
+
+vless_demo_1 = {
+    'type': 'vless',
+    'name': '🇭🇰 DataGrids DMIT XTLS 1Gbps',
+    'info': {
+        'server': '54.142.232.16',
+        'port': 52227,
+        'method': 'none',
+        'id': '75e6edee-2c30-41b0-9d88-676c4fb19ee8',
+        'stream': {
+            'type': 'tcp',
+            'obfs': None,
+            'secure': {
+                'type': 'xtls',
+                'sni': 'proxy.demo.com',
+                'alpn': None,
+                'verify': True,
+                'flow': 'xtls-direct',
+                'udp443': False
+            }
+        }
+    }
+}
+
+vless_demo_1['info'] = Filter('vless', vless_demo_1['info'])
+
+print(vless_demo_1)
